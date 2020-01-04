9019a2b4a5cb585cf3084fd7a3e8eb32f1036830
Vladimir Malzam <v.malzam@gmail.com>
2020-01-04 06:38:59 +0200

    checking the operation of the method "StringBuilder.reverse ()" on the condition that "all non-letter characters must remain in their original places"

---
+++ src/ua/com/foxminded/malzam/anagram/Anagram.java
@@ -0,0 +1,29 @@
+package ua.com.foxminded.malzam.anagram;
+
+import java.util.StringJoiner;
+
+public class Anagram {
+
+    public String reverseText(String text) {
+        StringJoiner joiner = new StringJoiner(" ");
+        for (String word : text.split(" ")) {
+            joiner.add(reverseLetters(word));
+        }
+        return joiner.toString();
+    }
+
+    private String reverseLetters(String word) {
+        char[] reversedWord = word.toCharArray();
+        for (int i = 0, j = word.length() - 1; i < j;) {
+            if (Character.isLetter(word.charAt(i))) {
+                if (Character.isLetter(word.charAt(j))) {
+                    reversedWord[i] = word.charAt(j);
+                    reversedWord[j] = word.charAt(i);
+                }
+                j--;
+            }
+            i++;
+        }
+        return new String(reversedWord);
+    }
+}
