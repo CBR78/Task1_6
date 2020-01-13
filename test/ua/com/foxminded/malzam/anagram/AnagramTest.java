eb7f51abcba342dd706c86cd257124fd6b790ed7
Vladimir Malzam <v.malzam@gmail.com>
2020-01-13 16:48:42 +0300

    Test names changed

--- test/ua/com/foxminded/malzam/anagram/AnagramTest.java
+++ test/ua/com/foxminded/malzam/anagram/AnagramTest.java
@@ -10,62 +10,62 @@ class AnagramTest {
     Anagram anagram = new Anagram();
 
     @Test
-    public void reverseText_Expect_IllegalArgumentException_IfInput_Null() {
+    public void reverseText_Expect_IllegalArgumentException_IfText_IsNull() {
         assertThrows(IllegalArgumentException.class, () -> anagram.reverseText(null));
     }
 
     @Test
-    public void reverseText_Expect_IllegalArgumentException_IfInput_Empty() {
+    public void reverseText_Expect_IllegalArgumentException_IfText_IsEmpty() {
         assertThrows(IllegalArgumentException.class, () -> anagram.reverseText(""));
     }
 
     @Test
-    public void reverseText_Expect_EmptyString_IfInput_OneSpace() {
+    public void reverseText_Expect_EmptyString_IfText_IsOneSpace() {
         assertEquals("", anagram.reverseText(" "));
     }
 
     @Test
-    public void reverseText_Expect_EmptyString_IfInput_TwoSpaces() {
+    public void reverseText_Expect_EmptyString_IfText_IsTwoSpaces() {
         assertEquals("", anagram.reverseText("  "));
     }
 
     @Test
-    public void reverseText_Expect_ReversedTwoLettersInUpperCase_IfInput_TwoLettersInUpperCase() {
+    public void reverseText_Expect_ReversedTwoLettersInUpperCase_IfText_IsTwoLettersInUpperCase() {
         assertEquals("BA", anagram.reverseText("AB"));
     }
 
     @Test
-    public void reverseText_Expect_ReversedTwoLettersInLowerCase_IfInput_TwoLettersInLowerCase() {
+    public void reverseText_Expect_ReversedTwoLettersInLowerCase_IfText_IsTwoLettersInLowerCase() {
         assertEquals("ba", anagram.reverseText("ab"));
     }
 
     @Test
-    public void reverseText_Expect_ThreeSymbols_IfInput_ThreeSymbols() {
+    public void reverseText_Expect_ThreeSymbols_IfText_IsThreeSymbols() {
         assertEquals("@#$", anagram.reverseText("@#$"));
     }
 
     @Test
-    public void reverseText_Expect_ThreeNumbers_IfInput_ThreeNumbers() {
+    public void reverseText_Expect_ThreeNumbers_IfText_IsThreeNumbers() {
         assertEquals("582", anagram.reverseText("582"));
     }
 
     @Test
-    public void reverseText_Expect_ReversedThreeLettersInDifferentCase_IfInput_ThreeLettersInDifferentCase() {
+    public void reverseText_Expect_ReversedThreeLettersInDifferentCase_IfText_IsThreeLettersInDifferentCase() {
         assertEquals("Cba", anagram.reverseText("abC"));
     }
 
     @Test
-    public void reverseText_Expect_TwoWordsFromSymbols_IfInput_TwoWordsFromSymbols() {
+    public void reverseText_Expect_TwoWordsFromSymbols_IfText_IsTwoWordsFromSymbols() {
         assertEquals("-+/* @#$%", anagram.reverseText("-+/* @#$%"));
     }
 
     @Test
-    public void reverseText_Expect_TwoWordsFromNumbers_IfInput_TwoWordsFromNumbers() {
+    public void reverseText_Expect_TwoWordsFromNumbers_IfText_IsTwoWordsFromNumbers() {
         assertEquals("12345 67890", anagram.reverseText("12345 67890"));
     }
 
     @Test
-    public void reverseText_Expect_ReversedTwoWordsFromLetters_IfInput_TwoWordsFromLetters() {
+    public void reverseText_Expect_ReversedTwoWordsFromLetters_IfText_IsTwoWordsFromLetters() {
         assertEquals("gfEDcba KjYHF", anagram.reverseText("abcDEfg FHYjK"));
     }
 
