e24f8fa7fe52b964f344179f824e8487d48c6260
Vladimir Malzam <v.malzam@gmail.com>
2020-01-13 17:25:47 +0300

    Renamed variables

--- src/ua/com/foxminded/malzam/anagram/Anagram.java
+++ src/ua/com/foxminded/malzam/anagram/Anagram.java
@@ -8,7 +8,7 @@ public class Anagram {
         if (text == null) {
             throw new IllegalArgumentException("text is illegal argument null");
         }
-        if ("".equals(text)) {
+        if (text.isEmpty()) {
             throw new IllegalArgumentException("text is empty");
         }
         StringJoiner joiner = new StringJoiner(" ");
@@ -19,24 +19,24 @@ public class Anagram {
     }
 
     private String reverseLetters(String word) {
-        char[] reversedWord = word.toCharArray();
+        char[] endChar = word.toCharArray();
         for (int i = 0, j = word.length() - 1; i < j;) {
-            char charI = word.charAt(i);
-            char charJ = word.charAt(j);
-            if (Character.isLetter(charI) && Character.isLetter(charJ)) {
-                reversedWord[i] = charJ;
-                reversedWord[j] = charI;
+            char startCharI = word.charAt(i);
+            char startCharJ = word.charAt(j);
+            if (Character.isLetter(startCharI) && Character.isLetter(startCharJ)) {
+                endChar[i] = startCharJ;
+                endChar[j] = startCharI;
                 i++;
                 j--;
             } else {
-                if (!Character.isLetter(charI)) {
+                if (!Character.isLetter(startCharI)) {
                     i++;
                 }
-                if (!Character.isLetter(charJ)) {
+                if (!Character.isLetter(startCharJ)) {
                     j--;
                 }
             }
         }
-        return new String(reversedWord);
+        return new String(endChar);
     }
 }
